18363dbc716807e86eb7d90dad9216ad9362c480
Weston Everett <westoneverett0@gmail.com>
2023-03-16 16:36:41 +0000

    test change

--- app/src/main/java/com/example/datacollectionpdr/DataCollection.java
+++ app/src/main/java/com/example/datacollectionpdr/DataCollection.java
@@ -45,17 +45,7 @@ public class DataCollection implements SensorEventListener {
     public void onSensorChanged(SensorEvent sensorEvent){
         switch (sensorEvent.sensor.getType()){
             case Sensor.TYPE_ACCELEROMETER:
-
-                gravity[0] = alpha * gravity[0] + (1 - alpha) * sensorEvent.values[0];
-                gravity[1] = alpha * gravity[1] + (1 - alpha) * sensorEvent.values[1];
-                gravity[2] = alpha * gravity[2] + (1 - alpha) * sensorEvent.values[2];
-
-                float linear_acceleration[] = new float[3];
-                linear_acceleration[0] = sensorEvent.values[0] - gravity[0];
-                linear_acceleration[1] = sensorEvent.values[1] - gravity[1];
-                linear_acceleration[2] = sensorEvent.values[2] - gravity[2];
-
-                motionSensorManagerListener.onAccValueUpdated(linear_acceleration);
+                motionSensorManagerListener.onAccValueUpdated(sensorEvent.values);
                 break;
 
             case Sensor.TYPE_GYROSCOPE:
