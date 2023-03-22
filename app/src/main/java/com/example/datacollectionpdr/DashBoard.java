86f14343cc24bd68aab915c2cb016e66d114b253
DagnaWojciak <s1859796@ed.ac.uk>
2023-03-22 21:48:18 +0000

    working toutorial example with menu

---
+++ app/src/main/java/com/example/datacollectionpdr/DashBoard.java
@@ -0,0 +1,47 @@
+package com.example.datacollectionpdr;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.dashboard);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.dashboard:
+                        return true;
+                    case R.id.about:
+                        startActivity(new Intent(getApplicationContext(),About.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/datacollectionpdr/MainActivity.java
+++ app/src/main/java/com/example/datacollectionpdr/MainActivity.java
@@ -2,15 +2,33 @@ package com.example.datacollectionpdr;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity implements com.example.datacollectionpdr.DataCollection.OnMotionSensorManagerListener{
 
     private com.example.datacollectionpdr.DataCollection mMotionSensorManager;
 
     private static final int REQUEST_ID_READ_WRITE_PERMISSION = 99;
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +37,36 @@ public class MainActivity extends AppCompatActivity implements com.example.datac
 
         mMotionSensorManager = new com.example.datacollectionpdr.DataCollection(this);
         mMotionSensorManager.setOnMotionSensorManagerListener(this);
-    }
+
+
+                // Initialize and assign variable
+                BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+                // Set Home selected
+                bottomNavigationView.setSelectedItemId(R.id.home);
+
+                // Perform item selected listener
+                bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        switch(item.getItemId())
+                        {
+                            case R.id.dashboard:
+                                startActivity(new Intent(getApplicationContext(),DashBoard.class));
+                                overridePendingTransition(0,0);
+                                return true;
+                            case R.id.home:
+                                return true;
+                            case R.id.about:
+                                startActivity(new Intent(getApplicationContext(),About.class));
+                                overridePendingTransition(0,0);
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+            }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
