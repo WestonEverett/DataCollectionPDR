240abbe61246680ed5780fbb8f937f63076c4c36
alexm <alex.miteloudis@gmail.com>
2023-03-22 12:46:12 +0000

    Sensor model & version function, added all sensors

--- app/src/main/java/com/example/datacollectionpdr/DataCollection.java
+++ app/src/main/java/com/example/datacollectionpdr/DataCollection.java
@@ -6,24 +6,49 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import java.util.Objects;
+
 public class DataCollection implements SensorEventListener {
 
     private OnMotionSensorManagerListener motionSensorManagerListener;
 
     private SensorManager sensorManager;
     private Sensor Accelerometer;
+    private Sensor AccelerometerUncalibrated;
     private Sensor Gyroscope;
-    private Sensor mMagneticField;
+    private Sensor GyroscopeUncalibrated;
+    private Sensor MagneticField;
+    private Sensor MagneticFieldUncalibrated;
     private Sensor Barometer;
+    private Sensor AmbientLight;
+    private Sensor Proximity;
+    private Sensor Gravity;
 
     public DataCollection(Context context){
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
 
-        mMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
-        Accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED);
-        Gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        //Initialize sensors
+        //Uncalibrated sensors for data submission
+        MagneticFieldUncalibrated = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
+        AccelerometerUncalibrated = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED);
+        GyroscopeUncalibrated = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        //Calibrated sensors for data processing
+        MagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         Barometer = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        AmbientLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        Proximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        Gravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
     }
+
+    // Function returns sensor vendor or version
+    public String sensorDetails(String query, int type){
+        String result = "";
+        if(Objects.equals(query, "Vendor")) result = sensorManager.getDefaultSensor(type).getVendor();
+        else if(Objects.equals(query, "Version")) result = String.valueOf(sensorManager.getDefaultSensor(type).getVersion());
+        return result;
+    };
 
     public void setOnMotionSensorManagerListener(OnMotionSensorManagerListener motionSensorManagerListener){
         this.motionSensorManagerListener = motionSensorManagerListener;
@@ -34,10 +59,16 @@ public class DataCollection implements SensorEventListener {
     }
 
     public void registerMotionSensors(){
-        sensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, Accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, Gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, Barometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, MagneticFieldUncalibrated, 10000); // 100 Samples/s
+        sensorManager.registerListener(this, AccelerometerUncalibrated, 10000); // 100 Samples/s
+        sensorManager.registerListener(this, GyroscopeUncalibrated, 10000); // 100 Samples/s
+        sensorManager.registerListener(this, MagneticField, 10000); // 100 Samples/s
+        sensorManager.registerListener(this, Accelerometer, 10000); // 100 Samples/s
+        sensorManager.registerListener(this, Gyroscope, 10000); // 100 Samples/s
+        sensorManager.registerListener(this, Barometer, 1000000); // 1 Sample/s
+        sensorManager.registerListener(this, AmbientLight, 1000000); // 1 Sample/s
+        sensorManager.registerListener(this, Proximity, 1000000); // 1 Sample/s
+        sensorManager.registerListener(this, Gravity, 10000); // 100 Samples/s
     }
 
     private double h;
@@ -47,6 +78,13 @@ public class DataCollection implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent sensorEvent){
         switch (sensorEvent.sensor.getType()){
+            case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+                h = Math.sqrt(sensorEvent.values[0] * sensorEvent.values[0] + sensorEvent.values[1] * sensorEvent.values[1] +
+                        sensorEvent.values[2] * sensorEvent.values[2]);
+
+                motionSensorManagerListener.onMagValueUpdated(sensorEvent.values, (float) h);
+                break;
+
             case Sensor.TYPE_ACCELEROMETER_UNCALIBRATED:
                 motionSensorManagerListener.onAccValueUpdated(sensorEvent.values);
                 break;
@@ -55,11 +93,32 @@ public class DataCollection implements SensorEventListener {
                 motionSensorManagerListener.onGyroValueUpdated(sensorEvent.values);
                 break;
 
-            case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
-                h = Math.sqrt(sensorEvent.values[0] * sensorEvent.values[0] + sensorEvent.values[1] * sensorEvent.values[1] +
-                        sensorEvent.values[2] * sensorEvent.values[2]);
+            case Sensor.TYPE_MAGNETIC_FIELD:
 
-                motionSensorManagerListener.onMagValueUpdated(sensorEvent.values, (float) h);
+                break;
+
+            case Sensor.TYPE_ACCELEROMETER:
+                //Do stuff
+                break;
+
+            case Sensor.TYPE_GYROSCOPE:
+                //Do other stuff
+                break;
+
+            case Sensor.TYPE_PRESSURE:
+
+                break;
+
+            case Sensor.TYPE_LIGHT:
+
+                break;
+
+            case Sensor.TYPE_PROXIMITY:
+
+                break;
+
+            case Sensor.TYPE_GRAVITY:
+
                 break;
 
         }
@@ -73,7 +132,46 @@ public class DataCollection implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        switch (sensor.getType()){
+            case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+                break;
 
+            case Sensor.TYPE_ACCELEROMETER_UNCALIBRATED:
+
+                break;
+
+            case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
+                break;
+
+            case Sensor.TYPE_MAGNETIC_FIELD:
+
+                break;
+
+            case Sensor.TYPE_ACCELEROMETER:
+                //Do stuff
+                break;
+
+            case Sensor.TYPE_GYROSCOPE:
+                //Do other stuff
+                break;
+
+            case Sensor.TYPE_PRESSURE:
+
+                break;
+
+            case Sensor.TYPE_LIGHT:
+
+                break;
+
+            case Sensor.TYPE_PROXIMITY:
+
+                break;
+
+            case Sensor.TYPE_GRAVITY:
+
+                break;
+
+        }
     }
 }
 
