23f2855df34cb1eff73b4290188e1d146f9f4e23
alexm <alex.miteloudis@gmail.com>
2023-03-23 18:12:38 +0000

    Sensor info for TrajectoryNative

--- app/src/main/java/com/example/datacollectionpdr/datacollectionandpreparation/DataManager.java
+++ app/src/main/java/com/example/datacollectionpdr/datacollectionandpreparation/DataManager.java
@@ -4,8 +4,11 @@ import android.util.Log;
 
 import com.example.datacollectionpdr.nativedata.LightData;
 import com.example.datacollectionpdr.nativedata.MotionSample;
+import com.example.datacollectionpdr.nativedata.PDRStep;
 import com.example.datacollectionpdr.nativedata.PositionData;
 import com.example.datacollectionpdr.nativedata.PressureData;
+import com.example.datacollectionpdr.nativedata.SensorDetails;
+import com.example.datacollectionpdr.nativedata.TrajectoryNative;
 import com.example.datacollectionpdr.nativedata.WifiSample;
 
 import java.util.HashMap;
@@ -14,6 +17,7 @@ public class DataManager extends PermissionsManager implements DataCollection.On
     private int stepcountDM;
     private MotionSample motionSample;
     private com.example.datacollectionpdr.datacollectionandpreparation.DataCollection mMotionSensorManager;
+    private TrajectoryNative trajectoryNative;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class DataManager extends PermissionsManager implements DataCollection.On
         motionSample = new MotionSample(System.currentTimeMillis(),stepcountDM);
         mMotionSensorManager = new com.example.datacollectionpdr.datacollectionandpreparation.DataCollection(this);
         mMotionSensorManager.setOnMotionSensorManagerListener(this);
+        trajectoryNative = new TrajectoryNative(System.currentTimeMillis());
     }
     @Override
     protected void onResume() {
@@ -36,6 +41,7 @@ public class DataManager extends PermissionsManager implements DataCollection.On
     public void onMagnetometerUncalibratedValueUpdated(float[] magneticfield, float h){
         //Log.i("DataM", "MagU data updated");
         PositionData positionData = new PositionData(System.currentTimeMillis(), magneticfield);
+        trajectoryNative.addPosition(positionData);
     }
     @Override
     public void onMagnetometerValueUpdated(float[] magneticfield, float h){
@@ -65,11 +71,13 @@ public class DataManager extends PermissionsManager implements DataCollection.On
     public void onBarometerValueUpdated(float pressure){
         //Log.i("DataM", "Bar data updated");
         PressureData pressureData = new PressureData(System.currentTimeMillis(), pressure);
+        trajectoryNative.addPressure(pressureData);
     }
     @Override
     public void onAmbientLightValueChanged(float luminance){
         //Log.i("DataM", "AmbL data updated");
         LightData lightData = new LightData(System.currentTimeMillis(),luminance);
+        trajectoryNative.addLight(lightData);
     }
     @Override
     public void onProximityValueUpdated(float proximity){
@@ -90,14 +98,17 @@ public class DataManager extends PermissionsManager implements DataCollection.On
        Log.i("DataM", "WiFi data updated");
        WifiSample wifiSample = new WifiSample(System.currentTimeMillis());
        wifiSample.addMacSampleDict(map);
+       trajectoryNative.addWifi(wifiSample);
     }
     @Override
     public void onStepDetectorUpdated(){
         //Log.i("DataM", "StpD data updated");
+        //PDRStep pdrStep = new PDRStep(1.f,2.f, System.currentTimeMillis());
     }
     @Override
     public void onStepCountValueUpdated(int stepcount){
         //Log.i("DataM", "StpC data updated");
+        motionSample.steps = stepcount-stepcountDM; //Step delta between new and old data
         stepcountDM = stepcount;
     }
     private void dealWithMotionSample(MotionSample motionSample){
@@ -105,7 +116,20 @@ public class DataManager extends PermissionsManager implements DataCollection.On
         if(motionSample.isComplete()){
             // Create new motionsample
             //Log.i("Motion sample", String.valueOf(System.currentTimeMillis()));
+            trajectoryNative.addMotion(motionSample);
             this.motionSample = new MotionSample(System.currentTimeMillis(),stepcountDM);
         }
     }
+    @Override
+    public void onSensorInfoCollected(SensorDetails accInfo, SensorDetails gyrInfo,
+                                      SensorDetails magInfo, SensorDetails barInfo,
+                                      SensorDetails ambInfo, SensorDetails rotInfo){
+        trajectoryNative.setAccInfo(accInfo);
+        trajectoryNative.setGyroInfo(gyrInfo);
+        trajectoryNative.setMagInfo(magInfo);
+        trajectoryNative.setBaroInfo(barInfo);
+        trajectoryNative.setLightInfo(ambInfo);
+        trajectoryNative.setRotVectorInfo(rotInfo);
+    }
+
 }
