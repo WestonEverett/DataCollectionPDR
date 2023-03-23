7361f97e18b9682a8be64ad5e6c56ee590c58393
Weston Everett <westoneverett0@gmail.com>
2023-03-23 00:56:34 +0000

    Completed (?) data serialization

--- app/src/main/java/com/example/datacollectionpdr/TrajectoryBuilder.java
+++ app/src/main/java/com/example/datacollectionpdr/TrajectoryBuilder.java
@@ -6,7 +6,11 @@ import com.example.datacollectionpdr.data.Light_Sample;
 import com.example.datacollectionpdr.data.Mac_Scan;
 import com.example.datacollectionpdr.data.Motion_Sample;
 import com.example.datacollectionpdr.data.Pdr_Sample;
+import com.example.datacollectionpdr.data.Position_Sample;
+import com.example.datacollectionpdr.data.Pressure_Sample;
+import com.example.datacollectionpdr.data.Sensor_Info;
 import com.example.datacollectionpdr.data.Trajectory;
+import com.example.datacollectionpdr.data.WiFi_Sample;
 
 public class TrajectoryBuilder {
 
@@ -18,6 +22,24 @@ public class TrajectoryBuilder {
         this.initTime = initTime;
         Trajectory.Builder trajectoryBuilder = Trajectory.newBuilder();
         trajectoryBuilder.setStartTimestamp(initTime);
+    }
+
+    public TrajectoryBuilder(long initTime, String androidVersion, String dataID){
+        this(initTime);
+        trajectoryBuilder.setAndroidVersion(androidVersion);
+        trajectoryBuilder.setDataIdentifier(dataID);
+    }
+
+    public Trajectory build(){
+        return trajectoryBuilder.build();
+    }
+
+    public void setDataID(String dataID){
+        trajectoryBuilder.setDataIdentifier(dataID);
+    }
+
+    public void setAndroidVerion(String androidVersion){
+        trajectoryBuilder.setAndroidVersion(androidVersion);
     }
 
     public void addPDR(float x, float y, long curTime){
@@ -59,15 +81,111 @@ public class TrajectoryBuilder {
         trajectoryBuilder.addLightData(newLight.build());
     }
 
-    public void addMac(long curTime, long mac, int rssi){
-        Mac_Scan.Builder newMac = Mac_Scan.newBuilder();
-        newMac.setMac(mac);
-        newMac.setRssi(rssi);
-        newMac.setRelativeTimestamp(curTime - initTime);
+    public void addWifi(long curTime, long[] mac, int[] rssi){
+        WiFi_Sample.Builder newWifi = WiFi_Sample.newBuilder();
+        long relTime = curTime - initTime;
+
+        newWifi.setRelativeTimestamp(relTime);
+
+        for(int i = 0; i < mac.length; i++){
+            Mac_Scan.Builder newMac = Mac_Scan.newBuilder();
+            newMac.setMac(mac[i]);
+            newMac.setRssi(rssi[i]);
+            newMac.setRelativeTimestamp(relTime);
+
+            newWifi.addMacScans(newMac.build());
+        }
+
+        trajectoryBuilder.addWifiData(newWifi.build());
     }
 
-    public void addMotion(){
+    public void addMotion(float[] acc, float[] gyro, float[] rotVector, int steps, long curTime){
         Motion_Sample.Builder newMotion = Motion_Sample.newBuilder();
+
+        newMotion.setAccX(acc[0]);
+        newMotion.setAccY(acc[1]);
+        newMotion.setAccZ(acc[2]);
+
+        newMotion.setGyrX(gyro[0]);
+        newMotion.setGyrY(gyro[1]);
+        newMotion.setGyrZ(gyro[2]);
+
+        newMotion.setRotationVectorX(rotVector[0]);
+        newMotion.setRotationVectorY(rotVector[1]);
+        newMotion.setRotationVectorZ(rotVector[2]);
+        newMotion.setRotationVectorW(rotVector[3]);
+
+        newMotion.setStepCount(steps);
+        newMotion.setRelativeTimestamp(curTime - initTime);
+
+        trajectoryBuilder.addImuData(newMotion.build());
     }
+
+    public void addPosition(long curTime, float[] mag){
+        Position_Sample.Builder newPosition = Position_Sample.newBuilder();
+        newPosition.setRelativeTimestamp(curTime - initTime);
+        newPosition.setMagX(mag[0]);
+        newPosition.setMagY(mag[1]);
+        newPosition.setMagZ(mag[2]);
+
+        trajectoryBuilder.addPositionData(newPosition.build());
+    }
+
+    public void addBaro(float pressure, long curTime){
+        Pressure_Sample.Builder newPressure = Pressure_Sample.newBuilder();
+        newPressure.setPressure(pressure);
+        newPressure.setRelativeTimestamp(curTime - initTime);
+
+        trajectoryBuilder.addPressureData(newPressure.build());
+    }
+
+    public void addAccInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info newSensor = createSensorInfo(name, vendor, res, power, version, type);
+
+        trajectoryBuilder.setAccelerometerInfo(newSensor);
+    }
+
+    public void addGyroInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info newSensor = createSensorInfo(name, vendor, res, power, version, type);
+
+        trajectoryBuilder.setGyroscopeInfo(newSensor);
+    }
+
+    public void addRotationVectorInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info newSensor = createSensorInfo(name, vendor, res, power, version, type);
+
+        trajectoryBuilder.setRotationVectorInfo(newSensor);
+    }
+
+    public void addMagnetomerInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info newSensor = createSensorInfo(name, vendor, res, power, version, type);
+
+        trajectoryBuilder.setMagnetometerInfo(newSensor);
+    }
+
+    public void addBaroInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info newSensor = createSensorInfo(name, vendor, res, power, version, type);
+
+        trajectoryBuilder.setBarometerInfo(newSensor);
+    }
+
+    public void addLightInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info newSensor = createSensorInfo(name, vendor, res, power, version, type);
+
+        trajectoryBuilder.setLightSensorInfo(newSensor);
+    }
+
+    private Sensor_Info createSensorInfo(String name, String vendor, float res, float power, int version, int type){
+        Sensor_Info.Builder newSensor = Sensor_Info.newBuilder();
+        newSensor.setName(name);
+        newSensor.setVendor(vendor);
+        newSensor.setResolution(res);
+        newSensor.setPower(power);
+        newSensor.setVersion(version);
+        newSensor.setType(type);
+
+        return newSensor.build();
+    }
+
 
 }
