796bef3a4a8fa3407a6bb960a1fea0643fb434dd
DagnaWojciak <s1859796@ed.ac.uk>
2023-03-24 01:15:23 +0000

    trying to add a map

--- app/src/main/java/com/example/datacollectionpdr/MapFragment.java
+++ app/src/main/java/com/example/datacollectionpdr/MapFragment.java
@@ -1,29 +1,60 @@
 package com.example.datacollectionpdr;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import com.example.datacollectionpdr.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
 
 public class MapFragment extends Fragment {
 
-    public MapFragment() {
-        // required empty public constructor.
-    }
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Initialize view
+        View view=inflater.inflate(R.layout.fragment_map, container, false);
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        // Initialize map fragment
+        SupportMapFragment supportMapFragment=(SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        // Async map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+                // Add a marker in Sydney and move the camera
+                LatLng sydney = new LatLng(-3.188267, -55.953251);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+                googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                googleMap.getUiSettings().setCompassEnabled(true);
+                googleMap.getUiSettings().setRotateGesturesEnabled(true);
+                googleMap.getUiSettings().setScrollGesturesEnabled(true);
+                googleMap.getUiSettings().setTiltGesturesEnabled(true);
+
+
+            }
+        });
+        // Return view
+        return view;
     }
 }
