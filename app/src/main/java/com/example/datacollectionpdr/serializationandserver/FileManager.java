db806b53dc05b554613c1bba65819534f2e21f1d
Weston Everett <westoneverett0@gmail.com>
2023-03-30 15:36:45 +0100

    stuff

--- app/src/main/java/com/example/datacollectionpdr/serializationandserver/FileManager.java
+++ app/src/main/java/com/example/datacollectionpdr/serializationandserver/FileManager.java
@@ -3,12 +3,19 @@ package com.example.datacollectionpdr.serializationandserver;
 import android.content.Context;
 import android.util.Log;
 
+import com.example.datacollectionpdr.data.Trajectory;
 import com.example.datacollectionpdr.nativedata.TrajectoryNative;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class FileManager {
@@ -24,6 +31,35 @@ public class FileManager {
         }
 
         Log.e("hm",dataFileName);
+    }
+
+    public static String[] seeFiles(Context context){
+        String[] filenames = context.fileList();
+        for(String str : filenames){
+            Log.e("LocalFile", str);
+        }
+
+        return filenames;
+    }
+
+    public static TrajectoryNative getTrajectoryFile(Context context, String filename) throws FileNotFoundException {
+        FileInputStream fis = context.openFileInput(filename);
+        InputStreamReader inputStreamReader =
+                new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            // Error occurred when opening raw file for reading.
+        } finally {
+            String contents = stringBuilder.toString();
+        }
+
+        return new TrajectoryNative(12);
     }
 
 }
--- app/src/main/java/com/example/datacollectionpdr/serializationandserver/ServerManager.java
+++ app/src/main/java/com/example/datacollectionpdr/serializationandserver/ServerManager.java
@@ -6,12 +6,7 @@ import android.util.Log;
 import com.example.datacollectionpdr.data.Trajectory;
 import com.example.datacollectionpdr.nativedata.TrajectoryNative;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -41,7 +36,6 @@ public class ServerManager {
 
         String fileName = "example.pkt";
         String fileType = "application/json";
-        String fileData = "Hello, world!";
 
         RequestBody body = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
@@ -56,60 +50,9 @@ public class ServerManager {
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         }
-
-
-        /*
-        Log.e("serverManager", "Bytes serialized");
-
-
-        // Create a URL object and open a connection
-        URL obj = new URL(url);
-
-        Log.e("serverManager", "URL Created");
-
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        Log.e("serverManager", "Connection Opened");
-
-        // Set the request method to POST
-        con.setRequestMethod("POST");
-
-        // Set the content type and content length headers
-        con.setRequestProperty("accept", "application/json");
-        con.setRequestProperty("Content-Type", "multipart/form-data");
-        con.setRequestProperty("file", "TrajectoryNewewewewewewe.pkt");
-
-        // Enable output and disable caching
-        con.setDoOutput(true);
-        con.setUseCaches(false);
-
-        serializedTraj.writeTo(con.getOutputStream());
-
-
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-
-        Log.e("serverManager", "Output Stream created");
-
-        wr.write(trajBytes);
-        wr.flush();
-        wr.close();
-
-        Log.e("serverManager", "Bytes Written");
-
-        // Read the response from the server
-        //BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        //String inputLine;
-        //StringBuffer response = new StringBuffer();
-        //while ((inputLine = in.readLine()) != null) {
-        //    response.append(inputLine);
-        //}
-        //in.close();
-
-        // Print the response from the server
-        //Log.e("serverManager", "server Response: " + response.toString());
-
-         */
-
     }
 
+    public TrajectoryNative getData(){
+        return new TrajectoryNative(12);
+    }
 }
